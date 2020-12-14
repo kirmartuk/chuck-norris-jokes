686f0adf134792b9955f68991c3988250fa91013
Kirill <kirillmartuk@gmail.com>
2020-12-15 01:26:47 +0700

    add all requared fragments

---
+++ app/src/main/java/com/martuk/testchuck/fragments/ApiDocumentationFragment.kt
@@ -0,0 +1,53 @@
+package com.martuk.testchuck.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import com.martuk.testchuck.R
+import kotlinx.android.synthetic.main.fragment_api_documentation.*
+
+class ApiDocumentationFragment : Fragment() {
+
+    companion object {
+
+        const val BASE_URL = "http://www.icndb.com/api/"
+
+        fun newInstance(): ApiDocumentationFragment {
+            return ApiDocumentationFragment()
+        }
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        wv_api.webViewClient = WebViewClient()
+        wv_api.loadUrl(BASE_URL)
+
+        // support onBackPressed in WebView
+        activity?.onBackPressedDispatcher?.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    when {
+                        wv_api.canGoBack() -> wv_api.goBack()
+                        else ->
+                            activity?.finish()
+                    }
+                }
+            })
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_api_documentation, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/martuk/testchuck/fragments/JokesFragment.kt
@@ -0,0 +1,74 @@
+package com.martuk.testchuck.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.martuk.testchuck.adapters.ChuckNorrisJokeRecyclerViewAdapter
+import com.martuk.testchuck.R
+import com.martuk.testchuck.api.LoadResult
+import com.martuk.testchuck.viewmodels.JokesViewModel
+import kotlinx.android.synthetic.main.custom_fab_widget.view.*
+import kotlinx.android.synthetic.main.fragment_jokes.*
+
+
+class JokesFragment : Fragment() {
+
+    private val mJokesViewModel: JokesViewModel by viewModels()
+
+    companion object {
+
+        fun newInstance(): JokesFragment {
+            return JokesFragment()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mJokesViewModel.jokes.observe(viewLifecycleOwner, {
+            rv_jokes.apply {
+                layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+                adapter = ChuckNorrisJokeRecyclerViewAdapter(it)
+            }
+
+        })
+        mJokesViewModel.progress.observe(viewLifecycleOwner, {
+            when (it) {
+                is LoadResult.Loading ->
+                    pgb_container.visibility = View.VISIBLE
+                is LoadResult.Success ->
+                    pgb_container.visibility = View.GONE
+                is LoadResult.Error -> {
+                    pgb_container.visibility = View.GONE
+                    Snackbar.make(view, getString(R.string.error), Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+        with(fab) {
+            ibtn_load.setOnClickListener {
+                text_layout.error = null
+                if (et_count_of_joke.text.toString().isEmpty()) {
+                    text_layout.error = context.getString(R.string.not_empty)
+                } else {
+                    mJokesViewModel.loadChuckNorrisJokes(et_count_of_joke.text.toString().toInt())
+
+                }
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_jokes, container, false)
+    }
+}
