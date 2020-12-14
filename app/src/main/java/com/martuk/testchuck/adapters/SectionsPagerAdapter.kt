1e502fa3636de4b0f141a58e2eac32c255bc3c02
Kirill <kirillmartuk@gmail.com>
2020-12-15 00:08:00 +0700

    move tab names from PageAdapter to MainActivity

--- app/src/main/java/com/martuk/testchuck/adapters/SectionsPagerAdapter.kt
+++ app/src/main/java/com/martuk/testchuck/adapters/SectionsPagerAdapter.kt
@@ -1,40 +1,32 @@
 package com.martuk.testchuck.adapters
 
 import android.content.Context
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.martuk.testchuck.R
 import com.martuk.testchuck.fragments.ApiDocumentationFragment
 import com.martuk.testchuck.fragments.JokesFragment
 
-private val TAB_TITLES = arrayOf(
-    R.string.tab_title_1,
-    R.string.tab_title_2
-)
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
  * one of the sections/tabs/pages.
  */
-class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
+class SectionsPagerAdapter(
+    private val mContext: Context,
+    fm: FragmentManager,
+    private val mTabTitles: Array<Int>
+) :
     FragmentPagerAdapter(fm) {
 
-    override fun getItem(position: Int): Fragment {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return when (position) {
-            0 -> JokesFragment.newInstance()
-            else -> ApiDocumentationFragment.newInstance()
-        }
+    override fun getItem(position: Int) = when (position) {
+        0 -> JokesFragment.newInstance()
+        else -> ApiDocumentationFragment.newInstance()
     }
+
 
     override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
+        return mContext.resources.getString(mTabTitles[position])
     }
 
-    override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
-    }
+    override fun getCount() = mTabTitles.size
 }
